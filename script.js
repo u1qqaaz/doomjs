53277343973e42916f15c2aec837502c6a28a7b0
u1qqaaz <130614920+u1qqaaz@users.noreply.github.com>
2023-05-03 16:10:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var dosbox = new Dosbox({
+ var dosbox = new Dosbox({
         id: "dosbox",
         onload: function (dosbox) {   dosbox.run("https://cdn.rawgit.com/darrengruber/docker-em-dosbox-doom/afad47b2/doom_shareware/doom19s_deiced.zip", "./DOOM.EXE");
         },
